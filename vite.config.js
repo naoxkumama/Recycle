f6f811e020bbf4200eeb71130a8aed93eca96e30
kumama <ten.years.new.decade@gmail.com>
2025-12-02 18:44:40 +0900

    update Vite config for HTTPS

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue';
 
 
 export default defineConfig({
-    base: '/build/',
+    base: 'https://recycle-api-staging.onrender.com/build/', 
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
